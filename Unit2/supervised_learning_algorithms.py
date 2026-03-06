0a3ca7cc415ffe04a29fddb0a416f6ee5fe55d68
ananyask1002 <int25ec421-t.ananya@nmit.ac.in>
2026-03-06 22:31:11 +0530

    Create supervised_learning_algorithms.py

---
+++ Unit2/supervised_learning_algorithms.py
@@ -0,0 +1,363 @@
+Example 1 : Sine Wave unit 1
+
+%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+# Generate a sequence of numbers from -10 to 10 with 100 steps in between
+x = np.linspace(-10, 10, 100)
+# Create a second array using sine
+y = np.sin(x)
+# The plot function makes a line chart of one array against another
+plt.plot(x, y, marker="x")
+
+Example 2 : Make forge
+
+import mglearn
+import matplotlib.pyplot as plt
+# generate dataset
+X, y = mglearn.datasets.make_forge()
+# plot dataset
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+plt.legend(["Class 0", "Class 1"], loc=4)
+plt.xlabel("First feature")
+plt.ylabel("Second feature")
+print("X.shape: {}".format(X.shape))
+
+Example 3 : Make wave
+
+X, y = mglearn.datasets.make_wave(n_samples=40)
+plt.plot(X, y, 'o')
+plt.ylim(-3, 3)
+plt.xlabel("Feature")
+plt.ylabel("Target")
+
+Example 4 : knn_classification
+
+mglearn.plots.plot_knn_classification(n_neighbors=1)
+# 3, 5, 7, 9
+
+
+Example 5 :  Train data using knn classifier
+
+from sklearn.model_selection import train_test_split
+X, y = mglearn.datasets.make_forge()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+from sklearn.neighbors import KNeighborsClassifier
+clf = KNeighborsClassifier(n_neighbors=3)
+
+clf.fit(X_train, y_train)
+
+print("Test set predictions: {}".format(clf.predict(X_test)))
+
+print("Test set accuracy: {:.2f}".format(clf.score(X_test, y_test)))
+
+
+Example 6 : Get 2D plots using axes for KNeighborsClassifier.
+
+fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+
+for n_neighbors, ax in zip([1, 3, 9], axes):
+	# the fit method returns the object self, so we can instantiate
+	# and fit in one line
+	clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
+	mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=.4)
+	mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+	ax.set_title("{} neighbor(s)".format(n_neighbors))
+	ax.set_xlabel("feature 0")
+	ax.set_ylabel("feature 1")
+axes[0].legend(loc=3)
+
+
+Example 7 : Breast_cancer_data
+
+from sklearn.datasets import load_breast_cancer
+
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(
+	cancer.data, cancer.target, stratify=cancer.target, random_state=66)
+
+training_accuracy = []
+test_accuracy = []
+# try n_neighbors from 1 to 10
+neighbors_settings = range(1, 11)
+
+for n_neighbors in neighbors_settings:
+	# build the model
+	clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+	clf.fit(X_train, y_train)
+	# record training set accuracy
+	training_accuracy.append(clf.score(X_train, y_train))
+	# record generalization accuracy
+	test_accuracy.append(clf.score(X_test, y_test))
+
+plt.plot(neighbors_settings, training_accuracy, label="training accuracy")
+plt.plot(neighbors_settings, test_accuracy, label="test accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("n_neighbors")
+plt.legend()
+
+Example 8 : KNN Regression
+
+mglearn.plots.plot_knn_regression(n_neighbors=1)
+(3,5,7,9)
+
+Example 9: sklearn knn regression
+
+from sklearn.neighbors import KNeighborsRegressor
+
+X, y = mglearn.datasets.make_wave(n_samples=40)
+
+# split the wave dataset into a training and a test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+# instantiate the model and set the number of neighbors to consider to 3
+reg = KNeighborsRegressor(n_neighbors=3)
+# fit the model using the training data and training targets
+reg.fit(X_train, y_train)
+
+print("Test set predictions:\n{}".format(reg.predict(X_test)))
+
+print("Test set R^2: {:.2f}".format(reg.score(X_test, y_test)))
+
+
+Example 10 : Analyzing KNN Regression
+
+fig, axes = plt.subplots(1, 3, figsize=(15, 4))
+# create 1,000 data points, evenly spaced between -3 and 3
+line = np.linspace(-3, 3, 1000).reshape(-1, 1)
+for n_neighbors, ax in zip([1, 3, 9], axes):
+	# make predictions using 1, 3, or 9 neighbors
+	reg = KNeighborsRegressor(n_neighbors=n_neighbors)
+	reg.fit(X_train, y_train)
+	ax.plot(line, reg.predict(line))
+	ax.plot(X_train, y_train, '^', c=mglearn.cm2(0), markersize=8)
+	ax.plot(X_test, y_test, 'v', c=mglearn.cm2(1), markersize=8)
+
+	ax.set_title("{} neighbor(s)\n train score: {:.2f} test score: {:.2f}".format(
+					n_neighbors, reg.score(X_train, y_train),
+					reg.score(X_test, y_test)))
+	ax.set_xlabel("Feature")
+	ax.set_ylabel("Target")
+axes[0].legend(["Model predictions", "Training data/target",
+				"Test data/target"], loc="best")
+				
+				
+Example 11: Linear regression
+
+mglearn.plots.plot_linear_regression_wave()
+
+Example 12: Linear regression
+
+from sklearn.linear_model import LinearRegression
+X, y = mglearn.datasets.make_wave(n_samples=60)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+lr = LinearRegression().fit(X_train, y_train)
+
+print("lr.coef_: {}".format(lr.coef_))
+print("lr.intercept_: {}".format(lr.intercept_))
+
+print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lr.score(X_test, y_test)))
+
+
+Example 13: Linear regression Boston data
+
+X, y = mglearn.datasets.load_extended_boston()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+lr = LinearRegression().fit(X_train, y_train)
+
+print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lr.score(X_test, y_test)))
+
+
+Example 14: Ridge samples
+
+mglearn.plots.plot_ridge_n_samples()
+
+
+Example 15: Ridge regression
+
+from sklearn.linear_model import Ridge
+
+#Ridge alpha value of 1
+ridge = Ridge().fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge.score(X_test, y_test)))
+
+#Ridge alpha value of 10
+ridge10 = Ridge(alpha=10).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge10.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge10.score(X_test, y_test)))
+
+#Ridge alpha value of 0.1
+ridge01 = Ridge(alpha=0.1).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge01.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge01.score(X_test, y_test)))
+
+#Print alpha value using plt
+plt.plot(ridge.coef_, 's', label="Ridge alpha=1")
+plt.plot(ridge10.coef_, '^', label="Ridge alpha=10")
+plt.plot(ridge01.coef_, 'v', label="Ridge alpha=0.1")
+plt.plot(lr.coef_, 'o', label="LinearRegression")
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient magnitude")
+plt.hlines(0, 0, len(lr.coef_))
+plt.ylim(-25, 25)
+plt.legend()
+
+
+
+
+Example 16: Lasso
+
+from sklearn.linear_model import Lasso
+lasso = Lasso().fit(X_train, y_train)
+print("Training set score: {:.2f}".format(lasso.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lasso.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lasso.coef_ != 0)))
+
+# we increase the default setting of "max_iter",
+# otherwise the model would warn us that we should increase max_iter.
+lasso001 = Lasso(alpha=0.01, max_iter=100000).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(lasso001.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lasso001.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lasso001.coef_ != 0)))
+
+
+lasso00001 = Lasso(alpha=0.0001, max_iter=100000).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(lasso00001.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lasso00001.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lasso00001.coef_ != 0)))
+
+
+plt.plot(lasso.coef_, 's', label="Lasso alpha=1")
+plt.plot(lasso001.coef_, '^', label="Lasso alpha=0.01")
+plt.plot(lasso00001.coef_, 'v', label="Lasso alpha=0.0001")
+plt.plot(ridge01.coef_, 'o', label="Ridge alpha=0.1")
+plt.legend(ncol=2, loc=(0, 1.05))
+plt.ylim(-25, 25)
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient magnitude")
+
+
+Example 17:Linear models for classification
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import train_test_split
+
+X, y = mglearn.datasets.make_forge()
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 3))
+
+for model, ax in zip([LinearSVC(), LogisticRegression()], axes):
+    clf = model.fit(X, y)
+    mglearn.plots.plot_2d_separator(clf, X, fill=False, eps=0.5, ax=ax, alpha=.7)
+    mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_title("{}".format(clf.__class__.__name__))
+    ax.set_xlabel("Feature 0")
+    ax.set_ylabel("Feature 1")
+
+axes[0].legend()
+
+mglearn.plots.plot_linear_svc_regularization()
+
+# split dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+logreg100 = LogisticRegression(C=100).fit(X_train, y_train)
+print("Training set score: {:.3f}".format(logreg100.score(X_train, y_train)))
+print("Test set score: {:.3f}".format(logreg100.score(X_test, y_test)))
+
+logreg001 = LogisticRegression(C=0.01).fit(X_train, y_train)
+print("Training set score: {:.3f}".format(logreg001.score(X_train, y_train)))
+print("Test set score: {:.3f}".format(logreg001.score(X_test, y_test)))
+
+Example 18: LogisticRegression
+
+rom sklearn.linear_model import LogisticRegression
+
+for C, marker in zip([0.001, 1, 100], ['o', '^', 'v']):
+    lr_l1 = LogisticRegression(C=C, penalty="l1", solver="liblinear", max_iter=5000).fit(X_train, y_train)
+
+    print("Training accuracy of l1 logreg with C={:.3f}: {:.2f}".format(
+        C, lr_l1.score(X_train, y_train)))
+
+    print("Test accuracy of l1 logreg with C={:.3f}: {:.2f}".format(
+        C, lr_l1.score(X_test, y_test)))
+
+    plt.plot(lr_l1.coef_.T, marker, label="C={:.3f}".format(C))
+
+plt.xticks(range(cancer.data.shape[1]), cancer.feature_names, rotation=90)
+plt.hlines(0, 0, cancer.data.shape[1])
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient magnitude")
+plt.ylim(-5, 5)
+plt.legend(loc=3)
+
+Example 19: Make_blobs
+
+from sklearn.datasets import make_blobs
+X, y = make_blobs(random_state=42)
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+plt.xlabel("Feature 0")
+plt.ylabel("Feature 1")
+plt.legend(["Class 0", "Class 1", "Class 2"])
+
+linear_svm = LinearSVC().fit(X, y)
+print("Coefficient shape: ", linear_svm.coef_.shape)
+print("Intercept shape: ", linear_svm.intercept_.shape)
+
+Example 20:
+
+import numpy as np
+import matplotlib.pyplot as plt
+import mglearn
+from sklearn.svm import LinearSVC
+
+# create synthetic dataset (3 classes)
+from sklearn.datasets import make_blobs
+X, y = make_blobs(random_state=42)
+
+# train Linear SVM model
+linear_svm = LinearSVC().fit(X, y)
+
+# scatter plot of data points
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+
+# create line space
+line = np.linspace(-15, 15)
+
+# plot decision boundaries
+for coef, intercept, color in zip(linear_svm.coef_, linear_svm.intercept_, ['b', 'r', 'g']):
+    plt.plot(line, -(line * coef[0] + intercept) / coef[1], c=color)
+
+# axis settings
+plt.ylim(-10, 15)
+plt.xlim(-10, 8)
+plt.xlabel("Feature 0")
+plt.ylabel("Feature 1")
+
+# legend
+plt.legend(['Class 0', 'Class 1', 'Class 2',
+            'Line class 0', 'Line class 1', 'Line class 2'],
+           loc=(1.01, 0.3))
+
+plt.show()
+
+
+Example 21:
+
+mglearn.plots.plot_2d_classification(linear_svm, X, fill=True, alpha=.7)
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+line = np.linspace(-15, 15)
+
+for coef, intercept, color in zip(linear_svm.coef_, linear_svm.intercept_, ['b','r','g']):
+    plt.plot(line, -(line * coef[0] + intercept) / coef[1], c=color)
+
+plt.xlabel("Feature 0")
+plt.ylabel("Feature 1")
+
